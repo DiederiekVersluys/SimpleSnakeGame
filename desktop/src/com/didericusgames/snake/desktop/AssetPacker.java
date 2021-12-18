6fab1ccc945b8629e69ae3763202a393d6e33153
DiederiekVersluys <80745300+DiederiekVersluys@users.noreply.github.com>
2021-12-18 13:25:35 +0100

    base game finished, now skinning it and adding sounds

---
+++ desktop/src/com/didericusgames/snake/desktop/AssetPacker.java
@@ -0,0 +1,25 @@
+package com.didericusgames.snake.desktop;
+
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+
+public class AssetPacker {
+
+    private static final String RAW_ASSETS_PATH = "C:\\gdxGame\\SimpleSnakeGame\\desktop\\assets-raw";
+    private static final String ASSETS_PATH = "android/assets";
+
+    public static void main(String[] args) {
+        TexturePacker.Settings settings = new TexturePacker.Settings();
+
+        TexturePacker.process(settings,
+                RAW_ASSETS_PATH + "/gameplay",
+                ASSETS_PATH + "/gameplay",
+                "gameplay"
+        );
+
+        TexturePacker.process(settings,
+                RAW_ASSETS_PATH + "/ui",
+                ASSETS_PATH + "/ui",
+                "ui"
+        );
+    }
+}
